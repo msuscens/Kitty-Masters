c594fc60a1b22d5fe611c52d29d9d618fa3dd73e
Mark Suscens <mark.suscens@gmail.com>
2021-07-04 10:25:00 +0100

    Prepare for debug repo pull and compile

--- migrations/3_market_migration.js
+++ migrations/3_market_migration.js
@@ -6,11 +6,18 @@ let marketInstance
 
 module.exports = async function (deployer, network, accounts) {
 
+  console.log("KittyContract.address:", KittyContract.address)
+  const kittyContract = await KittyContract.deployed()
+  console.log("kittyContract.address:", kittyContract.address)
+
+  console.log("***DEBUG : About to deployProxy")
   // Deploy the Logic contract and initialize (with associated proxy)
   marketInstance = await deployProxy(
     Marketplace, 
-    [KittyContract.address], 
+    [kittyContract.address], 
+    // [KittyContract.address], 
     { deployer, initializer: 'init_KittyMarketplace', from: accounts[0]}
   )
-  
+  console.log("***DEBUG:Done deployProxy")
+
 }
