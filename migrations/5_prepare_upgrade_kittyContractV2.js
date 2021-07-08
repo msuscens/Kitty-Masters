dc963bb471c6b6ff17418047cc9fc1af6e46a960
Mark Suscens <mark.suscens@gmail.com>
2021-07-08 13:22:40 +0100

    Comment out upgrade command - to avoid executing upgrade on every truffle migrate

--- migrations/5_prepare_upgrade_kittyContractV2.js
+++ migrations/5_prepare_upgrade_kittyContractV2.js
@@ -1,10 +1,15 @@
-// migrations/5_prepare_upgrade_kittyContractV2.js
 const KittyContract = artifacts.require('KittyContract')
 const KittyContractV2 = artifacts.require('KittyContractV2')
  
 const { prepareUpgrade } = require('@openzeppelin/truffle-upgrades')
  
 module.exports = async function (deployer) {
+
+  // COMMENTED OUT FOR NOW SO WE DON'T ACCIDENTIALLY 
+  // UPGRADE KittyContract TO KittyContractV2 
+  /*
   const kittyInstance = await KittyContract.deployed()
   await prepareUpgrade(kittyInstance.address, KittyContractV2, { deployer })
+  */
 }
+
--- migrations/6_prepare_upgrade_kittyMarketplaceV2.js
+++ migrations/6_prepare_upgrade_kittyMarketplaceV2.js
@@ -1,10 +1,14 @@
-// migrations/6_prepare_upgrade_kittyMarketplaceV2.js
 const Marketplace = artifacts.require('KittyMarketplace')
 const MarketplaceV2 = artifacts.require('KittyMarketplaceV2')
  
 const { prepareUpgrade } = require('@openzeppelin/truffle-upgrades')
  
 module.exports = async function (deployer) {
+
+  // COMMENTED OUT FOR NOW SO WE DON'T ACCIDENTIALLY 
+  // UPGRADE KittyMarketplace TO KittyMarketplaceV2 
+  /*
   const marketInstance = await Marketplace.deployed()
   await prepareUpgrade(marketInstance.address, MarketplaceV2, { deployer })
+  */
 }
