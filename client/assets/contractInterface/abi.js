793086b8a959159172fb4003049008b45e3a0673
Mark Suscens <mark.suscens@gmail.com>
2021-07-08 14:44:09 +0100

    Update to latest 'original' abi given that migration scripts no longer by default update contracts to V2

--- client/assets/contractInterface/abi.js
+++ client/assets/contractInterface/abi.js
@@ -537,6 +537,20 @@ const abi = {
       "type": "function"
     },
     {
+      "inputs": [],
+      "name": "pause",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "unpause",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
       "inputs": [
         {
           "internalType": "uint256",
