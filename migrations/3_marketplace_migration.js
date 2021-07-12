15376b1ad8264edc8ba99b68a4331237de5d0f0a
Mark Suscens <mark.suscens@gmail.com>
2021-07-12 19:00:21 +0100

    Rename variable to keep naming standard

--- migrations/3_marketplace_migration.js
+++ migrations/3_marketplace_migration.js
@@ -2,7 +2,7 @@ const { deployProxy } = require('@openzeppelin/truffle-upgrades')
 const DragonToken = artifacts.require("DragonToken")
 const Marketplace = artifacts.require("Marketplace")
 
-let marketInstance
+let marketplace
 
 module.exports = async function (deployer, network, accounts) {
 
@@ -10,7 +10,7 @@ module.exports = async function (deployer, network, accounts) {
   const dragonToken = await DragonToken.deployed()
 
   // Deploy the Marketplace (logic & proxy) and initialize
-  marketInstance = await deployProxy(
+  marketplace = await deployProxy(
     Marketplace, 
     [dragonToken.address], 
     { deployer, initializer: 'init_Marketplace', from: accounts[0]}
